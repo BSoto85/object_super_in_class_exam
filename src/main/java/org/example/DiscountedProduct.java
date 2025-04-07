a3e6b2bfac85bbdab059cf3a21cfa0a1289005ef
brenda-soto <bsoto@pursuit.org>
2025-04-07 16:07:26 -0400

    Finished object super in class exam

--- src/main/java/org/example/DiscountedProduct.java
+++ src/main/java/org/example/DiscountedProduct.java
@@ -1,5 +1,15 @@
 package org.example;
 
 public class DiscountedProduct extends Product {
+    double discountPercentage;
 
+    public DiscountedProduct(String name, double price, String description, double discountPercentage) {
+        super(name, price, description);
+        this.discountPercentage = discountPercentage;
+    }
+
+    public double calculateDiscountedPrice() {
+        double price = getPrice();
+        return price - (discountPercentage/100 * price);
+    }
 }
--- src/main/java/org/example/Product.java
+++ src/main/java/org/example/Product.java
@@ -1,5 +1,31 @@
 package org.example;
 
 public class Product {
+    private String name;
+    private double price;
+    private String description;
 
+    public Product(String name, double price, String description) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void displayProductInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Price: " + price);
+        System.out.println("Description: " + description);
+    }
 }
